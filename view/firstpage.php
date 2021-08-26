15e4d5a4b1562f83dbffabbfa5bdd4240388552f
Shan-Dilranga <2019cs187@stu.ucsc.lk>
2021-08-27 05:00:28 +0530

    modified

--- view/firstpage.php
+++ view/firstpage.php
@@ -5,7 +5,7 @@
     <title>Bloomfield</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="icon" href="images/bloomfieldlogo.png" sizes="32*32">
-    <link rel="stylesheet" href="includes/firstpage.css">
+    <link rel="stylesheet" href="style/firstpage.css">
   </head>
   <body>
     <header>
