c5edd4db0946f07fe3f044b024288ebedd905d2d
Gimhani-Rubasinghe <r.g.u.rubasinghe123@gmail.com>
2021-09-03 22:43:28 +0530

    Upload with Database with missing userlogin table

--- plunk.sql
+++ plunk.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 03, 2021 at 04:45 PM
+-- Generation Time: Sep 03, 2021 at 07:13 PM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.8
 
