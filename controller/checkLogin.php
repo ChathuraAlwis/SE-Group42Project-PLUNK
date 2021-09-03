28422861cff09438f6829c55b8977468f3d5d473
ChathuraAlwis <chathura7alwis@gmail.com>
2021-09-04 00:33:43 +0530

    show pages of any user type

--- controller/checkLogin.php
+++ controller/checkLogin.php
@@ -2,19 +2,17 @@
 
     if(isset($_POST['UserID']) && isset($_POST['password'])){
 
-        require_once '../model/models.php';
+        require_once '../model/users.php';
         require_once 'controller.php';
 
-        $model = new Model;
-        $result = $model->getLogin($_POST['UserID'], $_POST['password']);
+        $user = new User;
+        $userType = $user->getLogin($_POST['UserID'], $_POST['password']);
 
-        if($result == 'login'){
-            $page = new Page('../view/generalmanager.html');
+        if($userType != "invalid user"){
+            $page = new Page("../view/" . $userType . ".html");
             $page->show();
         }
         else{
-            // echo $result;
-            // include_once '../view/login.html';
             $page = new Page('../view/login.html');
             $page->show();
         }
