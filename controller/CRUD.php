3dc4d02fb3b0d9fe73d8990bf0349b15831263c4
ChathuraAlwis <chathura7alwis@gmail.com>
2021-09-10 01:38:50 +0530

    added add-order and delete-order to crud operations

---
+++ controller/CRUD.php
@@ -0,0 +1,29 @@
+<?php
+
+    require_once "../model/database.php";
+
+    if(isset($_POST['add-order'])){
+        $DB = new DB;
+
+        try {
+            $sql = "INSERT INTO plunk.order (Order_ID, Order_Place, Quantity, Member_ID, Staff_ID) VALUES ('$_POST[Order_ID]', '$_POST[Order_Place]', '$_POST[Quantity]', '$_POST[Member_ID]', '$_POST[Staff_ID]');";
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+
+        echo $DB->crud($sql);
+    }
+
+    if(isset($_POST['delete-order'])){
+        $DB = new DB;
+
+        try {
+            $sql = "DELETE FROM plunk.order WHERE Order_ID=\"$_POST[Order_ID]\"";
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+
+        echo $DB->crud($sql);
+    }
+
+?>
