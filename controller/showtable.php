3927911ad2ef525760de869cc82573846fd61ae1
ChathuraAlwis <chathura7alwis@gmail.com>
2021-09-28 03:18:14 +0530

    show required columns

--- controller/showtable.php
+++ controller/showtable.php
@@ -11,7 +11,6 @@
             $this->tableName = $table;
         }
 
-<<<<<<< HEAD
         function show($newQuery="", $all=true){
             if($all){
                 $sql = "SELECT * FROM plunk.$this->tableName";
@@ -20,11 +19,6 @@
                 $sql=$newQuery;
             }
             $result = $this->DB->runQuery($sql);
-=======
-        function show(){
-            $sql = "SELECT * FROM plunk.$this->tableName";
-          $result = $this->DB->runQuery($sql);
->>>>>>> a353f1cd88d649064dc3f13b83c3935a47f0a845
             $recordCount = count($result);
 
             $heading = false;
