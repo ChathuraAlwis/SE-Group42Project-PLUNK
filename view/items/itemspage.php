2cbddf363b06169fb8df5172fece32beb90792c2
GimhaniRubasinghe <2019cs148@stu.ucsc.lk>
2021-09-28 20:07:48 +0530

    reuploaded addgrn form

--- view/items/itemspage.php
+++ view/items/itemspage.php
@@ -6,7 +6,7 @@
         <title>Bloomfield</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="icon" type="icon" href="images/bloomfieldlogo.png" sizes="32*32">
-        <link rel="stylesheet" href="../style/stafftable.css">
+        <link rel="stylesheet" href="../style/tableui.css">
 
   </head>
   <body>
