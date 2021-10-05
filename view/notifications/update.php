bc49fc9c15a25a87972ea3ad3a713dea3b3a26d9
GimhaniRubasinghe <2019cs148@stu.ucsc.lk>
2021-10-05 23:08:49 +0530

    Uploaded updatenotification form

---
+++ view/notifications/update.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+
+        <meta charset="utf-8">
+        <title>Bloomfield</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="icon" href="images/bloomfieldlogo.png" sizes="32*32">
+        <link rel="stylesheet" href="../style/crud.css">
+        <script type="text/javascript" src="../script/additem.js"></script>
+
+  </head>
+  <body>
+    <div class="main">
+    <div class= "left">
+    <div class="form">
+        <h2 class="center-text"><b>Notifications details</b><image src = "../images/bin.png" class="bin"></image></h2>
+        <form action="../../controller/CRUD.php" method="POST">
+                <input name ="update-notifications" type="hidden" >
+               <div class="t"> <table class="formtable">
+               <tr>
+                    <div class="form-group">
+                        <td> <label for="NotificationID">Date</label></td>
+                        <td><input type="number" id= "NotificationID" name="NotificationID" required class="form-control" /></td>
+                    </div>
+                </tr>
+               <tr>
+                    <div class="form-group">
+                        <td> <label for="Date">Date</label></td>
+                        <td><input type="date" id= "Date" name="Date" required class="form-control" /></td>
+                    </div>
+                </tr>
+                <tr>
+                    <div class="form-group">
+                        <td><label for="EventType">Event Type</label></td>
+                        <td><select id="EventType" name="EventType" class="form-control"  onchange="changeType(this);">
+                            <option selected>Choose type...</option>
+                            <option value="Club">Club Event</option>
+                            <option value="Restaurant">Restaurant Event</option>
+                            </select>
+                        </td>
+                    </div>
+                </tr>
+                <tr>
+                <div class="form-group">
+                    <td><label for="Message">Message</label></td>
+                    <td><textarea  id= "Message" name="Message" required class="form-control" ></textarea></td>
+                </div>
+                </tr>
+            </table>
+        </div>
+                <div class="form-group">
+                    <button type="submit" name="submit" value="Submit" class="button submit">Update</button>
+                    <button type="submit" name="cancel" value="cancel" class="button submit"><a href="notificationtable.php">Cancel</a></button>
+                </div>
+        </form>        
+    </div>
+    </div>
+    <div class= "right">
+        <div class = "righttable">
+            <div class="itemtable">
+            <h3>NOTIFICATIONS TABLE</h3>
+            <iframe src="notificationtable.php" class="item"></iframe>
+            </div>
+        </div>
+    </div>
+
+</div> 
+  </body>
+
+ 
+</html>
