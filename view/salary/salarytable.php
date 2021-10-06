8b962849d945a7d42ffcf2a2d7c98f502f9c27e8
Pavani263 <2019cs093@stu.ucsc.lk>
2021-10-06 08:59:10 +0530

    Modified report UI

--- view/salary/salarytable.php
+++ view/salary/salarytable.php
@@ -16,7 +16,7 @@
               <div class="tableheader">
                     <div class="innerdiv">
                     </div>
-                    <h2>Salary</h2>
+                    <h2>Salarys</h2>
               </div>
             </div>
             <div class="covertable">
