a1859b6208a8a49de15cbb7cab9c519aa307d5ce
ChathuraAlwis <chathura7alwis@gmail.com>
2021-10-08 01:53:53 +0530

    disabled addidng duplicate items to item list

--- view/script/maxQuantity.js
+++ view/script/maxQuantity.js
@@ -1,12 +1,31 @@
 function maxQuantity(itemQuantity){
-    var ItemID = document.getElementById("ItemID").value;
+    var ItemID = document.getElementById("ItemID");
     var max = 0;
+    var canAdd = false;
+
     itemQuantity.forEach(element => {
         // console.log(element['ItemID'], ItemID);
-        if (element['ItemID'] == ItemID) {
+        if (element['ItemID'] == ItemID.value) {
             max = element['Quantity'];
+            canAdd = true;
         }
     });
+
+    var Table = document.getElementById("myTableData");
+
+    for (var r = 1, n = Table.rows.length; r < n; r++) {
+        var presentID = document.getElementById("ItemID"+r).value;
+        if(presentID == ItemID.value){
+            canAdd=false;
+        }
+    }
+
+    if(canAdd){
+        document.getElementById("add").disabled=false;
+    }
+    else{
+        document.getElementById("add").disabled=true;
+    }
     var Quantity = document.getElementById("Quantity");
     Quantity.setAttribute("max", max);
     // console.log(max);
