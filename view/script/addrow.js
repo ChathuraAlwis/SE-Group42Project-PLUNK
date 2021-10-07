42fa0cc6f51c661646562bde3e19abdc4a4f4be0
ChathuraAlwis <chathura7alwis@gmail.com>
2021-10-08 01:51:55 +0530

    updated for finding reducing correct price when delleting and changed table data order

--- view/script/addrow.js
+++ view/script/addrow.js
@@ -1,7 +1,4 @@
- var rows = 0;
-
- function addRowOrder(itemPrice) {    
-    rows++;     
+ function addRowOrder(itemPrice) {       
     var ItemID = document.getElementById("ItemID"); 
     var Quantity = document.getElementById("Quantity");
     var Total = document.getElementById("Total");
@@ -9,33 +6,32 @@
         var table = document.getElementById("myTableData"); 
         var rowCount = table.rows.length; 
         var row = table.insertRow(rowCount); 
-        row.insertCell(0).innerHTML= '<input type="button" value = "Delete" onClick="Javacsript:deleteRowOrder(this)">'; 
-        row.insertCell(1).innerHTML= '<input type="text" value = "' + ItemID.value + '" name="ItemID'+ rows +'" style="border:none" size=5 readonly >'; 
-        row.insertCell(2).innerHTML= '<input type="text" value = "' + Quantity.value + '" name="Quantity'+ rows +'" style="border:none" size=5 readonly >';
-
+        row.insertCell(0).innerHTML= '<input type="text" value = "' + ItemID.value + '" name="ItemID'+ rowCount + '" id="ItemID' + rowCount + '" style="border:none" size=5 readonly >'; 
+        row.insertCell(1).innerHTML= '<input type="text" value = "' + Quantity.value + '" name="Quantity'+ rowCount + '" style="border:none" size=5 readonly >';
+        
         itemPrice.forEach(element => {
             // console.log(element['ItemID'], ItemID);
             if (element['ItemID'] == ItemID.value) {
                 var tot = element['Price'] * Quantity.value;
                 var Tot = parseInt(Total.value) + tot;
-                row.insertCell(3).innerHTML= '<input type="text" value = "' + tot + '" id="Price'+ rows +'" style="border:none" size=5 readonly >';
+                row.insertCell(2).innerHTML= '<input type="text" class="Price" value = "' + tot + '" id="Price'+ rowCount +'" style="border:none" size=5 readonly >';
                 Total.setAttribute("value", Tot); 
             }
         });
         
+        row.insertCell(3).innerHTML= '<input type="button" value = "Delete" onClick="Javacsript:deleteRowOrder(this)">'; 
+        document.getElementById("add").setAttribute("disabled", true);
     }
 } 
  
-function deleteRowOrder(obj) {    
-    rows--; 
-    var index = obj.parentNode.parentNode.rowIndex; 
-    var table = document.getElementById("myTableData");
+function deleteRowOrder(obj) {  
+    var index = obj.parentNode.parentNode.rowIndex;   
+    var Table = document.getElementById("myTableData");
     var Total = document.getElementById("Total");
-    var price = document.getElementById("Price" + index).value;
-    // console.log(price.value);
-    var Tot = Total.value - price;
+    var Price = Table.rows[index].getElementsByClassName("Price")[0].value;
+    var Tot = Total.value - Price;
     Total.setAttribute("value", Tot);
-    table.deleteRow(index);    
+    Table.deleteRow(index);
 } 
 
 
