5f614b00ff5c552e503193579918032b82f49ab3
Pavani263 <2019cs093@stu.ucsc.lk>
2021-10-07 18:41:36 +0530

    Created staff Member UI

--- view/Dashboards/staffdash.php
+++ view/Dashboards/staffdash.php
@@ -8,6 +8,7 @@
         <meta name="viewport" content="width=device-width,height=device-height, initial-scale=1">
         <link rel="icon" type="icon" href="../images/bloomfieldlogo.png" sizes="32*32">
         <link rel="stylesheet" href="../style/Dashboard.css">
+        <link rel="stylesheet" href="../style/tableui.css">
 
 
 
@@ -15,12 +16,21 @@
     <body>
         <div class="main">
           <div class="upper">
-                <div class="restbooking">
+                <div class="resbooking">
+                <h3>Bookings</h3>
+                <div class="detailtable">
 
+                    <iframe src="../staffmember/books.php" class="staff"></iframe>
+                    </div>
                 </div>
-                <div class="clubooking">
 
-                </div>
+                <div class="reorder">
+                  <h3>Items</h3>
+                  <div class="detailtable">
+
+                    <iframe src="../staffmember/books.php" class="staff"></iframe>
+                    </div>
+                    </div>
                 <div class="calendar">
                     <iframe class="bloomcal" src="https://calendar.google.com/calendar/embed?height=500&wkst=2&bgcolor=%23009688&ctz=Asia%2FColombo&showTz=0&showPrint=0&title=Bloomfield%20Calendar&showTabs=0&showDate=1&showTitle=1&src=NG5vZWhmNTQzMXZoaGtidDRnczdoYTJ0N29AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=ZW4ubGsjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23B39DDB&color=%230B8043" style="border-width:0" width="500" height="500" frameborder="0" scrolling="no"></iframe>
                 </div>
@@ -29,8 +39,11 @@
               <h2>Notifications</h2>
           </div>
           <div class="Notifications">
-
-
+          
+          <div class="detailtable">
+            <iframe src="../restaurantmanager/notificationrm.php" class="staff"></iframe>
+          
+          </div>
           </div>
 
 
