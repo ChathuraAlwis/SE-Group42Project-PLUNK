935b7e4bfab98d372892406d0e62e9c3cf4e10de
GimhaniRubasinghe <2019cs148@stu.ucsc.lk>
2021-10-09 01:07:31 +0530

    Recorrected update reservation menu form

--- view/reservationmenu/update.php
+++ view/reservationmenu/update.php
@@ -8,9 +8,23 @@
         <link rel="icon" type="icon" href="images/bloomfieldlogo.png" sizes="32*32">
         <link rel="stylesheet" href="../style/crud.css">
         <script type="text/javascript" src="../script/additem.js"></script>
+        <script type="text/javascript" src="../script/updatedata.js"></script>
 
   </head>
   <body>
+  <script>
+        var ans = getParameterByName('data', window.location.href);
+        var id = ans.slice(-1);
+        document.cookie = 'data='+id;
+    </script>
+    <?php 
+        require_once "../../model/database.php";
+        $DB = new DB;
+        $name = $_COOKIE['data'];
+        $query = "SELECT * FROM plunk.resevationmenu WHERE ReservationName=$name";
+        $result = $DB->runQuery($query)[0];
+        //print_r($result);
+    ?>
     <div class=main>
     <div class= left>
     <div class="form">
@@ -20,13 +34,13 @@
             <table>
                 <tr><div class="form-group">
                         <td><label for="ReservationName">Reservation Name</label></td>
-                        <td><input type="text" id= "ReservationName" name="ReservationName" required class="form-control" /></td>
+                        <td><input type="text" id= "ReservationName" name="ReservationName" required class="form-control" value = "<?php echo "$result[ReservstionName]";?>"/></td>
                     </div>
                 </tr>
                 <tr><td><br></td></tr>
                 <tr><div class="form-group">
                     <td><label for="Type">Reservation Type</label></td>
-                    <td><select id="Type" name="Type" class="form-control" placeholder="Enter the item type" onchange="changeType(this);">
+                    <td><select id="Type" name="Type" class="form-control" placeholder="Enter the item type" value = "<?php echo "$result[Type]";?>" onchange="changeType(this);">
                     <option selected> Choose type ...</option>
                         <option value="club">Club</option>
                         <option value="restaurant">Restaurant</option>
@@ -37,13 +51,13 @@
                 <tr>
                 <div class="form-group">
                     <td><label for="Cost">Cost</label></td>
-                    <td><input type="number" id="Cost" name="Cost" required class="form-control" /></td>
+                    <td><input type="number" id="Cost" name="Cost" required class="form-control" value = "<?php echo "$result[Cost]";?>"/></td>
                     </div>
                 </tr>
             </table>
                 <div class="form-group">
                     <button type="submit" name="submit" value="Submit" class="button submit">update</button>
-                    <button type="submit" name="cancel" value="cancel" class="button submit"><a href="Reservationmenutable.php">Cancel</a></button>
+                    <button type="submit" name="cancel" value="cancel" class="button submit"><a href="Reservationmenupage.php">Cancel</a></button>
                 </div>
         </form> 
     </div>       
