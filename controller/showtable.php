e20ded0e5d9a0aa19a5638b12d4105fc768f1c78
ChathuraAlwis <chathura7alwis@gmail.com>
2021-10-11 20:48:11 +0530

    changed show function to direct to different pages when clicked on data

--- controller/showtable.php
+++ controller/showtable.php
@@ -13,15 +13,8 @@
             $this->tableName = $table;
         }
 
-        function show($newQuery="", $all=true){
-            if($all){
-                //default query to show all detail of the table
-                $sql = "SELECT * FROM plunk.$this->tableName";
-            }
-            else{
-                //custom query
-                $sql = $newQuery;
-            }
+        function show($sql, $linkPage){
+
             //get the returned array with data from database
             $result = $this->DB->runQuery($sql);
 
@@ -48,7 +41,7 @@
                         //prepare data to send through get method for update forms
                         $record = http_build_query(array('record' => $result[$row]));
                         //link each data in the table with the update form
-                        echo "<td><a href=\"update.php?data=$record\" target=\"Pages\">$data</a></td>";
+                        echo "<td><a href=\"" . $linkPage . ".php?data=$record\" target=\"Pages\">$data</a></td>";
                     }
                 }
                 //end row
