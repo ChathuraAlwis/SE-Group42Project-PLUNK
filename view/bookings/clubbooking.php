ea4ac3f6393644c5ea418d8c047622e45b20c0f4
ChathuraAlwis <chathura7alwis@gmail.com>
2021-10-12 05:25:16 +0530

    show bookings for club member

---
+++ view/bookings/clubbooking.php
@@ -0,0 +1,30 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+
+        <meta charset="utf-8">
+        <title>Bloomfield</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="icon" href="images/bloomfieldlogo.png" sizes="32*32">
+        <link rel="stylesheet" href="../style/tabledesign.css">
+
+  </head>
+  <body>
+        <div class="main" >
+
+                    <div class="detailtable">
+                      <?php
+                          require_once "..\..\controller\showtable.php";
+                          $orderTable = new Table("booking");
+                          $orderTable->show("SELECT CustomerName as Name, ReservedDate as 'Reserved Date',Total as 'Total Payment',Payment as Paid, ContactNo as 'Contact No' FROM plunk.booking WHERE BookingType=1");
+                       ?>
+
+
+                    </div>
+
+                </div>
+
+
+  </body>
+</html>
