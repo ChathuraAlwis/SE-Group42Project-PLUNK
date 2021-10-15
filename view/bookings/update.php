c8abf28620de9fd26e3834e2de1566e8a03f1cbd
Shan-Dilranga <2019cs187@stu.ucsc.lk>
2021-10-15 11:18:40 +0530

    modified bookings

--- view/bookings/update.php
+++ view/bookings/update.php
@@ -10,26 +10,41 @@
 
   </head>
   <body>
+    <?php
+        require_once "../../model/database.php";
+        $DB = new DB;
+        $id = explode("=", $_GET['data'])[1];
+        $query = "SELECT * FROM plunk.booking WHERE BookingID='$id'";
+
+        $result = $DB->runQuery($query)[0];
+
+    ?>
         <div class="main" >
             <div class="header">
               <div class="upperrow"></div>
               <h2>Bookings</h2>
 
             </div>
-            <form class="addbooking" action="index.html" method="post" autocomplete="on" >
+            <form class="addbooking" action="..\..\controller\CRUD.php" method="post" autocomplete="on" >
+              <input name ="update-booking" type="hidden" >
+              <div class="headerrow">
+                <div class="bin">
+                  <a href="delete.php?data=<?php echo $_GET['data'];?>" ><img src="..\images\bin.png" alt="Delete Icon" class="binicon"></a>
+                </div>
+              </div>
               <div class="submain">
                 <div class="questions">
                     <label for="BookingID">Booking ID :</label>
-                    <input type="text" name="BookingID" readonly>
+                    <input type="text" name="BookingID" value = "<?php echo "$result[BookingID]";?>" readonly>
                 </div><br>
                   <div class="questions">
                       <label for="CustomerName">Name :</label>
-                      <input type="text" name="CustomerName" required>
+                      <input type="text" name="CustomerName" value = "<?php echo "$result[CustomerName]";?>" required>
                   </div><br>
                   <div class="questions">
                     <label for="type">Booking Type   :</label>
-                    <select class="type" name="BookingType" required>
-
+                    <select class="type" name="BookingType"  required>
+                            <?php echo "$result[BookingType]";?>
                             <option value="Club">Club</option>
                             <option value="Restaurant">Restaurant</option>
                     </select>
@@ -49,15 +64,15 @@
                   </div><br>
                   <div class="questions">
                         <label for="date">Reserved Date :</label>
-                        <input type="date" name="ReservedDate" required>
+                        <input type="date" name="ReservedDate" value = "<?php echo "$result[ReservedDate]";?>" required>
                   </div><br>
                   <div class="questions">
-                        <label for="time">Reserved Time :</label>
-                        <input type="time" name="ReservedTime" required>
+                        <label for="time">Reserved Time:</label>
+                        <input type="time" name="ReservedTime" value = "<?php echo "$result[ReservedTime]";?>" required>
                   </div><br>
                   <div class="questions">
                         <label for="contactno">Contact No :</label>
-                        <input type="tel" name="ContactNo" required>
+                        <input type="tel" name="ContactNo" value = "<?php echo "$result[ContactNo]";?>" required>
                   </div><br>
 
                 </div>
