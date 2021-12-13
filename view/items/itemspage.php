9721a47fb0f7f5c69578956641a78a29175d1127
ChathuraAlwis <chathura7alwis@gmail.com>
2021-12-14 01:12:37 +0530

    item search function

--- view/items/itemspage.php
+++ view/items/itemspage.php
@@ -1,61 +1,83 @@
 <?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
+<head>
 
-        <meta charset="utf-8">
-        <title>Bloomfield</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" type="icon" href="images/bloomfieldlogo.png" sizes="32*32">
-        <link rel="stylesheet" href="../style/stafftable.css">
+      <meta charset="utf-8">
+      <title>Bloomfield</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="icon" type="icon" href="images/bloomfieldlogo.png" sizes="32*32">
+      <link rel="stylesheet" href="../style/stafftable.css">
 
-  </head>
-  <body>
-        <div class="main" >
+</head>
+<body>
+      <div class="main" >
             <div class="coverheader">
-
-              <div class="tableheader">
-                    <div class="innerdiv">
-                    </div>
-                    <h2>ITEMS</h2>
-              </div>
+                  <div class="tableheader">
+                        <div class="innerdiv">
+                        </div>
+                        <h2>ITEMS</h2>
+                  </div>
             </div>
             <div class="covertable">
-                <div class="table">
-                    <div>
-                    <input type = "hidden" class = "search" value="search-item"/>
-                    <div >
-                              <input type = "text" id= "ItemID" name= "ItemID" class = "search" placeholder="Enter the item name" />
-                  </div>
-                    <div >
-                              <button type = "submit" class = "search" ><a href= "#" ><b>Search</b></a></button>
+                  <div class="table">
+                        <div class="upperbar">
+                              <form method="POST" action="itemspage.php">
+                                    <input type = "hidden" class = "search" name="itemsearch"/>
+                                    <div >
+                                          <input type = "text" id= "ItemName" name= "ItemName" class = "search" placeholder="Search By Item Name" value="<?php if(isset($_POST['itemsearch'])) {echo $_POST['ItemName'];}?>" />
+                                    </div>
+                                    <div >
+                                          <select id="ItemType" name="ItemType" class="search" placeholder="Enter the item type" onchange="changeType(this);">
+                                                <?php 
+                                                      if(isset($_POST['itemsearch'])){
+                                                            if ($_POST['ItemType']=='0'){
+                                                                  echo '<option value="0" selected>Item Type: All</option>
+                                                                  <option value="1">Item Type: Food Items</option>
+                                                                  <option value="2">Item Type: Beverage Items</option>';
+                                                            }elseif ($_POST['ItemType']=='1') {
+                                                                  echo '<option value="0">Item Type: All</option>
+                                                                  <option value="1" selected>Item Type: Food Items</option>
+                                                                  <option value="2">Item Type: Beverage Items</option>';
+                                                            }else{
+                                                                  echo '<option value="0">Item Type: All</option>
+                                                                  <option value="1">Item Type: Food Items</option>
+                                                                  <option value="2" selected>Item Type: Beverage Items</option>';
+                                                            }
+                                                      }
+                                                      else{
+                                                            echo '<option value="0" selected>Item Type: All</option>
+                                                            <option value="1">Item Type: Food Items</option>
+                                                            <option value="2">Item Type: Beverage Items</option>';
+                                                      }
+                                                      ?>
+                                          </select>
+                                    </div>
+                                    <div >
+                                          <button type = "submit" class = "search" ><b>Search</b></button>
+                                    </div>
+                              </form>
+                              <?php
+                                    if ($_SESSION['UserType']=='Restaurant Manager'){
+                                          echo "<div class=\"addicon\">
+                                                      <a href=\"../items/additems.php\" class=\"add\"><button type=\"button\" name=\"button\" class=\"addbtn\"><b>+</b></button></a>
+                                                </div>";
+                                    }               
+                              ?>
                         </div>
-                        <div >
-                              <select id="ItemType" name="ItemType" class="search" placeholder="Enter the item type" onchange="changeType(this);">
-                                    <option selected>Choose item type...</option>
-                                    <option value="Food">Food Items</option>
-                                    <option value="Beverage">Beverage Items</option>
-                              </select>
-                        </div>   
-                        <?php
-                              if ($_SESSION['UserType']=='Restaurant Manager'){
-                                    echo "<div class=\"addicon\">
-                                                <a href=\"../items/additems.php\" class=\"add\"><button type=\"button\" name=\"button\" class=\"addbtn\"><b>+</b></button></a>
-                                          </div>";
-                              }               
-                        ?>
-                    </div>
-                    <div class="detailtable">
-
-                        <iframe src="itemtable.php" class="staff"></iframe>
+                        <div class="detailtable">
+                              <?php 
+                                    if(isset($_POST['itemsearch'])){
+                                          echo '<iframe src="itemtable.php?name=' . $_POST['ItemName'] . '&type='. $_POST['ItemType'] .'" class="staff"></iframe>';
+                                    }
+                                    else{
+                                          echo '<iframe src="itemtable.php" class="staff"></iframe>';
+                                    }
+                              ?>
+                        </div>
                   </div>
-                </div>
-
             </div>
-
-
-        </div>
-
-  </body>
+      </div>
+</body>
 </html>
 
