0147b7fa919ff0904c58e02f321ec893ac0e32d7
Shan-Dilranga <2019cs187@stu.ucsc.lk>
2021-12-24 01:40:52 +0530

    created manage company functionality, modified sign up functionality

--- view/user/decision.php
+++ view/user/decision.php
@@ -8,7 +8,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width,height=device-height, initial-scale=1">
         <link rel="icon" type="icon" href="../images/bloomfieldlogo.png" sizes="32*32">
-        <link rel="stylesheet" href="../style/signup.css">
+        <link rel="stylesheet" href="..\style\decision.css">
 
 
 
@@ -27,9 +27,9 @@
     <div class="main">
 
 
-              <div class="middle">
+              <div class="middle2">
 
-                            <div class="mainpages" id="mainpages">
+                            <div class="mainpage3" id="mainpages">
                               <div class="formbox">
                                 <form class="adduser" action="..\..\controller\CRUD.php" method="post" autocomplete="on" >
                                   <input name ="joinrequest" type="hidden" >
@@ -37,15 +37,15 @@
                                     <div class="imagebox">
                                       <input type="file"  accept="image/*" name="image" id="file"  onchange="loadFile(event)" style="display: none;" required>
 
-                                      <img id="output" >
-                                      <label for="file" class="upload"><b>Upload Image</b> </label>
+                                      <?php echo '<img id="output" alt="No Profile Picture" src="data:image/jpeg;base64,'.base64_encode($result['ProfilePic']).'"/>';?>
+                                      <!-- <label for="file" class="upload"><b>Upload Image</b> </label>
 
                                       <script>
                                       var loadFile = function(event) {
                                       	var image = document.getElementById('output');
                                       	image.src = URL.createObjectURL(event.target.files[0]);
                                       };
-                                      </script>
+                                      </script> -->
 
                                     </div><br>
                                     <div class="forminputs">
@@ -54,8 +54,8 @@
                                     </div>
 
                                     <div class="radio">
-                                          <label for="UserType">Requesting Member Type</label><br>
-                                          <input type="text" class="input" name="UserType" value="" readonly>
+                                          <label for="UserType">Requested Member Type</label><br>
+                                          <input type="text" class="input" name="UserType" value="<?php echo "$result[UserType]";?>" readonly>
                                           <!-- <select class="UserType" name="UserType" id="UserType" readonly>
                                                   <option selected>Select the member type</option>
                                                   <option value="Ordinary Member">Ordinary Member</option>
@@ -65,22 +65,22 @@
                                     </div>
 
                                     <div class="forminputs">
-                                        <label for="JoinedYear"> Requesting date</label><br>
-                                        <input type="date" id="JoinedYear" class="input" name="JoinedYear" value="" readonly>
+                                        <label for="JoinedYear"> Requested date</label><br>
+                                        <input type="date" id="JoinedYear" class="input" name="JoinedYear" value="<?php echo "$result[JoinedYear]";?>" readonly>
                                     </div>
                                     <div class="forminputs">
                                         <label for="Email"> E-mail </label><br>
-                                        <input type="email" id="Email" class="input" name="Email" placeholder="xxxx@gmail.com" value="" readonly>
+                                        <input type="email" id="Email" class="input" name="Email" placeholder="xxxx@gmail.com" value="<?php echo "$result[Email]";?>" readonly>
                                     </div>
                                     <div class="forminputs">
                                         <label for="ContactNo"> Contact No</label><br>
-                                        <input type=" tel" id="ContactNo" class="input" name="ContactNo" pattern="[0-9]{10}" value="" readonly>
+                                        <input type=" tel" id="ContactNo" class="input" name="ContactNo" pattern="[0-9]{10}" value="<?php echo "$result[ContactNo]";?>" readonly>
                                     </div><br>
 
 
                                     <div class="formbtn">
                                       <button type="button"  id="approve" class="add" name="button"  ><a href="approve.php?data=<?php echo $_GET['data'];?>" class="btnlink">Approve</a></button>
-                                      <button type="button" id="deny" class="add" name="button"><a href="deny.php" class="btnlink">Deny</a></button>
+                                      <button type="button" id="deny" class="add" name="button"><a href="deny.php?data=<?php echo $_GET['data'];?>" class="btnlink">Deny</a></button>
                                     </div>
 
                                   </div>
