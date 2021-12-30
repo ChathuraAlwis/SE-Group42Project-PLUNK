4e13fa7be3b5e5aa9428a76fefb0b503c045e9ee
ChathuraAlwis <chathura7alwis@gmail.com>
2021-12-30 14:06:19 +0530

    service charge report

--- controller/showreport.php
+++ controller/showreport.php
@@ -45,5 +45,49 @@ class Report{
         echo "</table>";
         // echo "Total = $totSum";
     }
+
+    function serviceChargeReport($start, $end){
+
+        echo "<table border=1 width=100% >";
+        
+        $names = $this->DB->runQuery("SELECT Distinct UserID, Name FROM plunk.servicechargestaff INNER JOIN plunk.user WHERE servicechargestaff.StaffID=user.UserID;");
+        $nameCol = array();
+        $result = $this->DB->runQuery("SELECT Date, Name, Amount, Percentage FROM plunk.servicecharge INNER JOIN (SELECT * FROM plunk.servicechargestaff INNER JOIN plunk.user WHERE servicechargestaff.StaffID=user.UserID) as userServiceCharge WHERE servicecharge.ServiceChargeID=userServiceCharge.ServiceChargeID AND Date BETWEEN '$start' AND '$end' ORDER BY Date;");
+        $cols = count($names);
+        echo "<tr><td></td>";
+        for ($col=0; $col<$cols; $col++) {
+            $name = $names[$col]['Name'];
+            array_push($nameCol, $name);
+            $id = $names[$col]['UserID'];
+            echo "<th> $name ($id)</th>";
+        }
+        echo "<th>Total</th></tr>";
+        $date = "1";
+        $tableRow = 0;
+        for ($dataRow=0; $dataRow<count($result); $dataRow++) {
+            $rowSum=0;
+            if($date != $result[$dataRow]['Date']){
+                $date = $result[$dataRow]['Date'];
+                echo "<tr><td> $date </td>";
+                $index = 0;
+                for ($col=$dataRow; $col<$cols+$dataRow; $col++) {
+                    if($date ==  $result[$col]['Date']){
+                        $amount = $result[$col]['Amount'];
+                        $per = $result[$col]['Percentage'];
+                    }
+                    else {
+                        $amount = $per = 0;
+                    }
+                    $rowSum += $amount;
+                    echo "<td> $amount ($per%) </td>";
+                }
+                echo "<td>$rowSum</td></tr>";
+            }else{
+                
+            }
+        }
+
+        echo "</table>";
+    }
 }
 ?>
