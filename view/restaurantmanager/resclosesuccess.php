4f54fda35031279934b6b46c720dcdc60a0070ee
GimhaniRubasinghe <2019cs148@stu.ucsc.lk>
2022-01-04 22:16:28 +0530

    uploaded restaurant status chnaging success msg files

---
+++ view/restaurantmanager/resclosesuccess.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+
+        <meta charset="utf-8">
+        <title>Bloomfield</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="icon" href="images/bloomfieldlogo.png" sizes="32*32">
+        <link rel="stylesheet" href="../style/success.css">
+        <script type="text/javascript" src="../script/usernavbtn.js"> </script>
+
+  </head>
+  <body>
+        <div class="main" >
+
+                    <div class="detailtable">
+                    <h3><b>Restaurant is closed now...</b> </h3>
+                    <h1><b>Updated..!</b> </h1><br>
+
+                    <div class="btn">
+                      <a href="../Dashboards/rmDashboard.php"><button type="refresh" name="button" ><b>OK</b> </button></a>
+                    </div>
+
+
+                    </div>
+
+                </div>
+
+
+  </body>
+</html>
